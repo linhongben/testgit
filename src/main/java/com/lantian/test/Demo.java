31c057cb27a5dd758eba7472ccf3c5ca8afa6649
lhben <lantianben@foxmail.com>
2018-12-15 21:53:38 +0800

    小贵子是个傻逼

---
+++ src/main/java/com/lantian/test/Demo.java
@@ -0,0 +1,14 @@
+package com.lantian.test;
+
+/**
+ * @author bianshanben
+ * @version v1.0
+ * @ClassName:Demo
+ * @Description:TODO
+ * @date 2018/12/15
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("小贵子是个傻逼");
+    }
+}
